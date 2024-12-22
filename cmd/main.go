92a51ed8987a51141d0cf1209bcde8a85c227b8d
Sultan <sultan_2191@mail.ru>
2024-12-22 16:11:29 +0500

    sdfasdf

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"forum/database"
 	"forum/handlers"
+	"forum/handlers/auth"
+	"forum/handlers/comment"
+	"forum/handlers/others"
+	"forum/handlers/post"
 	"net/http"
 )
 
@@ -35,4 +39,38 @@ func main() {
 	if err != nil {
 		fmt.Println("Error starting HTTPS server:", err)
 	}
+}
+func routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./web/css"))))
+	mux.Handle("/web/images/", http.StripPrefix("/web/images/", http.FileServer(http.Dir("./web/images"))))
+	mux.HandleFunc("/", post.MainPage)
+	mux.HandleFunc("/sign_in", auth.SignIn)
+	mux.HandleFunc("/sign_up", auth.SignUp)
+	mux.HandleFunc("/sign_out", auth.SignOut)
+	mux.HandleFunc("/create_post", post.CreatePost)
+	mux.HandleFunc("/my_posts", post.MyPosts)
+	mux.HandleFunc("/posts/", post.Post)
+	mux.HandleFunc("/category/", post.Filter)
+	mux.HandleFunc("/react", post.React)
+	mux.HandleFunc("/liked_posts", post.LikedPosts)
+	mux.HandleFunc("/delete_post", post.DeletePost)
+	mux.HandleFunc("/comment", comment.Comment)
+	mux.HandleFunc("/react_comment", comment.ReactComment)
+	mux.HandleFunc("/delete_comment", comment.DeleteComment)
+	mux.HandleFunc("/login", auth.GoogleLogin)
+	mux.HandleFunc("/auth/callback", auth.GoogleCallback)
+	mux.HandleFunc("/login_github", auth.GithubLogin)
+	mux.HandleFunc("/auth/github/callback", auth.GitHubCallback)
+	mux.HandleFunc("/user_profile", others.Profile)
+	mux.HandleFunc("/apply", others.Apply)
+	mux.HandleFunc("/admin_page", others.AdminPage)
+	mux.HandleFunc("/admin_page_allow", others.ModeratorAllow)
+	mux.HandleFunc("/admin_page_deny", others.ModeratorDeny)
+	mux.HandleFunc("/admin_page_demote_to_user", others.DemoteToUser)
+	mux.HandleFunc("/report_to_admin", others.ReportToAdmin)
+	mux.HandleFunc("/send_reply", others.SendReply)
+	mux.HandleFunc("/delete_report_from_admin", others.DeleteReportFromAdminPage)
+	mux.HandleFunc("/delete_reply_from_admin", others.DeleteReplyByID)
+	return mux
 }
